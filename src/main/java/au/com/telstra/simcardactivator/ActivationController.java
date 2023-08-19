95af275b657afb100cbc641ab4fff44d6db3b6da
d963244 <dion.kouskouris@team.telstra.com>
2023-08-19 12:18:16 +1000

    Adding post request mapping

--- src/main/java/au/com/telstra/simcardactivator/ActivationController.java
+++ src/main/java/au/com/telstra/simcardactivator/ActivationController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 public class ActivationController {
     @PostMapping("/actuate")
     public String receiveData(@RequestBody SIMInformation simInformation){
-
         if (simInformation.getIccid() instanceof String && simInformation.getCustomerEmail() instanceof String) {
             return "success: " + true;
         }
